de6dc49377a7b46862c485a110cf5cff88686a62
HabtamuArgeta <128905774+HabtamuArgeta@users.noreply.github.com>
2025-09-30 17:37:33 +0300

    Add migrate_changes.py

---
+++ migrate_changes.py
@@ -0,0 +1,14 @@
+import os
+import django
+from django.core.management import call_command
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+django.setup()
+
+# Make migrations for all installed apps
+call_command('makemigrations')
+
+# Apply migrations for all apps
+call_command('migrate')
+
+print("All apps migrated successfully!")
