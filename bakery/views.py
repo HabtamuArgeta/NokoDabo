260788506bac79e222b5b1332e85a44e7c7881f8
HabtamuArgeta <128905774+HabtamuArgeta@users.noreply.github.com>
2025-09-30 17:37:49 +0300

    Add bakery/views.py

---
+++ bakery/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+from .models import Bread, Injera, WheatFlour, Yeast, Enhancer
+from .forms import StockTransactionForm
+
+def home(request):
+    return render(request, "home.html")
+
+@require_GET
+def get_products(request):
+    product_type = request.GET.get("product_type")
+    products = []
+    if product_type == "bread":
+        products = list(Bread.objects.values("id", "name"))
+    elif product_type == "injera":
+        products = list(Injera.objects.values("id", "name"))
+    elif product_type == "flour":
+        products = list(WheatFlour.objects.values("id", "name"))
+    elif product_type == "yeast":
+        products = list(Yeast.objects.values("id", "name"))
+    elif product_type == "enhancer":
+        products = list(Enhancer.objects.values("id", "name"))
+    return JsonResponse({"products": products})
