8fff3936800c38a689ecd63252be06e03e157f27
HabtamuArgeta <128905774+HabtamuArgeta@users.noreply.github.com>
2025-10-01 10:37:18 +0300

    V1.1.3

--- migrate_changes.py
+++ migrate_changes.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NokoDabo.settings')
 django.setup()
 
 # Make migrations for all installed apps.
