f9eb06f86979fd897676adc4ff2706ba114b4d2e
HabtamuArgeta <128905774+HabtamuArgeta@users.noreply.github.com>
2025-10-03 15:37:05 +0300

    V1110

--- finance/apps.py
+++ finance/apps.py
@@ -1,5 +1,14 @@
+# finance/apps.py
 from django.apps import AppConfig
 
 class FinanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'finance'
+
+    def ready(self):
+        # import signals (safe import)
+        try:
+            import finance.signals  # noqa
+        except Exception:
+            # if signals fail, we don't want to crash startup — log in real app
+            pass
