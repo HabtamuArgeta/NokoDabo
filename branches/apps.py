6d818472b0484c7870737be5dc12a703e905b7e7
HabtamuArgeta <128905774+HabtamuArgeta@users.noreply.github.com>
2025-10-04 19:38:41 +0300

    V1112

--- branches/apps.py
+++ branches/apps.py
@@ -1,5 +1,13 @@
+# branches/apps.py
 from django.apps import AppConfig
 
 class BranchesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'branches'
+
+    def ready(self):
+        # import signals so they are registered
+        try:
+            import branches.signals  # noqa
+        except Exception:
+            pass
